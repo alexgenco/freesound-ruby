eefbddb0756a410f147cd97460d772cb54f4cf96
Alex Genco <alexgenco@gmail.com>
2013-12-05 14:42:31 -0800

    filter freesound keys from vcr cassette yml

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,12 +1,14 @@
 $:.unshift File.expand_path("../lib", __FILE__)
-require 'rspec/autorun'
-require 'vcr'
+require "rspec/autorun"
+require "vcr"
 
 RSpec.configure do |config|
   config.before { Freesound.api_key = ENV.fetch("FREESOUND_KEY") }
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+
+  config.filter_sensitive_data("<FREESOUND_KEY>") { ENV.fetch("FREESOUND_KEY") }
 end
