7af5dcdb19c8330b2ce15273d3a003b797e22c3e
Alex Genco <alexgenco@gmail.com>
2013-12-05 16:58:18 -0800

    dry up relationship methods. add some docs (how do you write docs)

--- lib/freesound/resources.rb
+++ lib/freesound/resources.rb
@@ -4,19 +4,55 @@ module Freesound
       self.site = "http://www.freesound.org/api"
       self.include_format_in_path = false
 
-      # The Freesound API requires URLs to have a trailing slash
-      # before their query parameters, otherwise the request will
-      # result in a redirect.
-      #
-      # These are hacks to construct the URLs correctly because
-      # ActiveResource does not append a trailing slash.
       class << self
+        # The Freesound API requires URLs to have a trailing slash before their
+        # query parameters, otherwise the request will result in a redirect.
+        #
+        # These are hacks to construct the URLs correctly because ActiveResource
+        # does not append a trailing slash.
+        #
         def element_path(id, *args)
           super("#{id}/", *args)
         end
 
         def get(path, *args)
           super("#{path}/", *args)
+        end
+
+        # Macro for establishing relationships.
+        #
+        # Resources return has_many relationships as a URL, rather than a
+        # collection of objects. For example user JSON would look like:
+        #
+        #   {
+        #     username: "alexgenco",
+        #     sounds: "http://www.freesound.org/api/people/alexgenco/sounds/"
+        #   }
+        #
+        # This macro generates methods to access these child resources by
+        # following the URL and instantiating the appropriate resource.
+        #
+        # To setup a User that has many sounds:
+        #
+        #   class User
+        #     references(:sounds, Freesound::Resources::Sound)
+        #   end
+        #
+        #   User.find("alexgenco").sounds # => an array of Sound objects
+        #
+        def references(collection_name)
+          resource_class = collection_name.to_s.singularize.camelize
+
+          class_eval(<<-RUBY, __FILE__, __LINE__)
+            def #{collection_name}(refresh=false)
+              @#{collection_name} = nil if refresh
+
+              @#{collection_name} ||= begin
+                path = URI.parse(super).path
+                #{resource_class}.find(:all, from: path, params: query_params)
+              end
+            end
+          RUBY
         end
       end
 
@@ -30,26 +66,15 @@ module Freesound
     class User < Base
       self.collection_name = "people"
 
+      references :sounds
+      references :packs
+
+      # User resources are identified by their username, for example:
+      #
+      #   /api/people/alexgenco
+      #
       def id
         username
-      end
-
-      def sounds(refresh=false)
-        @sounds = nil if refresh
-
-        @sounds ||= begin
-          path = URI.parse(super).path
-          Sound.find(:all, from: path, params: query_params)
-        end
-      end
-
-      def packs(refresh=false)
-        @packs = nil if refresh
-
-        @packs ||= begin
-          path = URI.parse(super).path
-          Pack.find(:all, from: path, params: query_params)
-        end
       end
     end
 
@@ -60,14 +85,7 @@ module Freesound
     class Pack < Base
       self.collection_parser = "Freesound::Collections::Packs"
 
-      def sounds(refresh=false)
-        @sounds = nil if refresh
-
-        @sounds ||= begin
-          path = URI.parse(super).path
-          Sound.find(:all, from: path, params: query_params)
-        end
-      end
+      references :sounds
     end
   end
 end
