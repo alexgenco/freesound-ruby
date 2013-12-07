714416b4593311e682fe88158453af68bf9a1516
Alex Genco <alexgenco@gmail.com>
2013-12-06 16:11:40 -0800

    pull __collection_key dynamically

--- lib/freesound/collections.rb
+++ lib/freesound/collections.rb
@@ -5,25 +5,22 @@ module Freesound
     # Collections are required to tell ActiveResource how to parse JSON
     # responses that wrap a collection of resources.
     #
-    # The #collection_key method provides the JSON key that will contain the
+    # The #__collection_key method determines the JSON key containing the
     # resource data.
     #
     class Base < ActiveResource::Collection
       def initialize(parsed={})
-        @elements = parsed.fetch(collection_key)
+        @elements = parsed.fetch(__collection_key)
+      end
+
+      private
+
+      def __collection_key
+        self.class.name.underscore
       end
     end
 
-    class Sounds < Base
-      def collection_key
-        "sounds"
-      end
-    end
-
-    class Packs < Base
-      def collection_key
-        "packs"
-      end
-    end
+    class Sounds < Base; end
+    class Packs  < Base; end
   end
 end
